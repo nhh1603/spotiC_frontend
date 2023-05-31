47b1035084ae83da2d0b7590bbc2621a237ec124
nhh1603 <nghiahieu12@gmail.com>
2023-05-31 15:31:45 +0200

    authentification page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Auth from "./pages/Auth";
 
 //functions
 import { getTest } from './functions/test';
 
-
+/*
 function App() {
 
     const [data, setData] = useState("Hello World!");
@@ -24,6 +28,17 @@ function App() {
             </header>
         </div>
     );
+}
+*/
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth" element={<Auth />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
