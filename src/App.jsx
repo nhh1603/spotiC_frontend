e9b47877f5abcd8d0a7f2fb05bc679b56295e4f7
nhh1603 <nghiahieu12@gmail.com>
2023-06-07 22:22:57 +0200

    Add Not Found Page, update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,28 @@
 import './App.css';
-import { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
+import { useDispatch } from "react-redux";
 
 //pages
 import AuthPage from "./pages/AuthPage";
 import LandingPage from './pages/LandingPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 //functions
-import { getTest } from './functions/test';
-
-/*
-function App() {
-
-    const [data, setData] = useState("Hello World!");
-
-    useEffect(() => {
-        getTest()
-            .then(data => setData(data.message))
-            .catch(error => console.log(error));
-    }, []);
-
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    {data}
-                </p>
-            </header>
-        </div>
-    );
-}
-*/
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
 
-                <Route path="/auth" element={<AuthPage />} />
-            </Routes>
-        </BrowserRouter>
+    return (
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+
+            <Route path="/auth" element={<AuthPage />} />
+
+            <Route path="/not-found" element={<NotFoundPage />} />
+
+            <Route path="*" element={<Navigate to="/not-found" />} />
+            
+        </Routes>
     )
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,9 +4,9 @@ import '../styles/LandingPage.css'; // Import the CSS file for the component
 function LandingPage () {
     return (
         <div className="animated-background">
-            <div className="container">
-                <h1>SpotiC</h1>
-                <h2>Where you can listen to your favorite music, free and without ads</h2>
+            <div className="landing-container">
+                <h1 id="landing-h1">SpotiC</h1>
+                <h2 id="landing-h2">Where you can listen to your favorite music, free and without ads</h2>
                 <br /> <br /> 
                 <span><a href="/auth"></a></span>
             </div>
